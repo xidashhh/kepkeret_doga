61f9f347764cf47d1a2dda6e9c8633aa85c41f0a
xidashhh <shadeeygg@gmail.com>
2022-10-15 10:44:35 +0200

    url változtatás

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import './style.css';
 
+
+
+function urlValtozas(){
+    let kepValue = document.getElementById('kepURL').value
+    document.getElementById('kep').src = kepValue
+    // console.log(kepValue)
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let kepURL = document.getElementById('kepURL')
+
+
+    kepURL.addEventListener('change', urlValtozas)
+    kepURL.addEventListener('change', urlValtozas)
+})
